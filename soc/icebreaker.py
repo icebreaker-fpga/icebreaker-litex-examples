09383272e2abc9cd382f58f2c31ff33f5db2b5e2
Piotr Esden-Tempski <piotr@esden.net>
2020-03-12 11:13:28 -0700

    Jump to address backported from upstream, upstreamed memory.x generator.
    Additionally also added the --flash soc target.

--- soc/icebreaker.py
+++ soc/icebreaker.py
@@ -22,15 +22,16 @@ import argparse
 from migen import *
 from migen.genlib.resetsync import AsyncResetSynchronizer
 
-from litex.soc.cores import up5kspram, spi_flash
+from litex.soc.cores.up5kspram import Up5kSPRAM
+from litex.soc.cores.spi_flash import SpiFlash
 from litex.soc.integration.soc_core import SoCCore
 from litex.soc.integration.builder import Builder, builder_argdict, builder_args
+from litex.build.lattice.programmer import IceStormProgrammer
 from litex.soc.integration.soc_core import soc_core_argdict, soc_core_args
 from litex.soc.integration.doc import AutoDoc
 
 from litex_boards.platforms.icebreaker import Platform
 
-from litex.soc.interconnect import wishbone
 from litex.soc.cores.uart import UARTWishboneBridge
 from rtl.leds import Leds
 
@@ -38,15 +39,6 @@ import litex.soc.doc as lxsocdoc
 from litex.soc.integration import export
 from litex.build.tools import write_to_file
 
-
-class JumpToAddressROM(wishbone.SRAM):
-    def __init__(self, size, addr):
-        data = [
-            0x00000537 | ((addr & 0xfffff000) << 0),   # lui   a0,%hi(addr)
-            0x00052503 | ((addr & 0x00000fff) << 20),  # lw    a0,%lo(addr)(a0)
-            0x000500e7,                                # jalr  a0
-        ]
-        wishbone.SRAM.__init__(self, size, read_only=True, init=data)
 
 # CRG ----------------------------------------------------------------------------------------------
 
@@ -83,14 +75,13 @@ class BaseSoC(SoCCore):
 
     # Statically-define the memory map, to prevent it from shifting across various litex versions.
     SoCCore.mem_map = {
-        "rom":              0x00000000,  # (default shadow @0x80000000)
         "sram":             0x10000000,  # (default shadow @0xa0000000)
         "spiflash":         0x20000000,  # (default shadow @0xa0000000)
         "csr":              0xe0000000,  # (default shadow @0x60000000)
         "vexriscv_debug":   0xf00f0000,
     }
 
-    def __init__(self, debug=True, boot_vector=0x20040000, **kwargs):
+    def __init__(self, debug, flash_offset, **kwargs):
         """Create a basic SoC for iCEBreaker.
 
         Create a basic SoC for iCEBreaker.  The `sys` frequency will run at 12 MHz.
@@ -100,11 +91,12 @@ class BaseSoC(SoCCore):
         """
         platform = Platform()
 
-        kwargs["cpu_variant"]       = "lite"
-        kwargs["cpu_reset_address"] = boot_vector
-        if debug:
-            kwargs["uart_name"]   = "crossover"
-            kwargs["cpu_variant"] = "lite+debug"
+        # Set cpu name and variant defaults when none are provided
+        if "cpu_variant" not in kwargs:
+            if debug:
+                kwargs["cpu_variant"] = "lite+debug"
+            else:
+                kwargs["cpu_variant"] = "lite"
 
         clk_freq = int(12e6)
 
@@ -112,32 +104,34 @@ class BaseSoC(SoCCore):
         kwargs["integrated_sram_size"] = 0
         kwargs["integrated_rom_size"]  = 0
 
-        SoCCore.__init__(self, platform, clk_freq, **kwargs)
+        # Set CPU reset address
+        kwargs["cpu_reset_address"] = self.mem_map["spiflash"] + flash_offset
 
-        # If there is a VexRiscv CPU, add a fake ROM that simply tells the CPU
-        # to jump to the given address.
-        if hasattr(self, "cpu") and self.cpu.name == "vexriscv":
-            self.add_memory_region("rom", 0, 16)
-            self.submodules.rom = JumpToAddressROM(16, boot_vector)
+        # SoCCore
+        SoCCore.__init__(self, platform, clk_freq, **kwargs)
 
         self.submodules.crg = _CRG(platform)
 
         # UP5K has single port RAM, which is a dedicated 128 kilobyte block.
         # Use this as CPU RAM.
         spram_size = 128 * 1024
-        self.submodules.spram = up5kspram.Up5kSPRAM(size=spram_size)
+        self.submodules.spram = Up5kSPRAM(size=spram_size)
         self.register_mem("sram", self.mem_map["sram"], self.spram.bus, spram_size)
 
         # The litex SPI module supports memory-mapped reads, as well as a bit-banged mode
         # for doing writes.
-        spi_pads = platform.request("spiflash4x")
-        self.submodules.lxspi = spi_flash.SpiFlash(spi_pads, dummy=6, endianness="little")
-        self.register_mem("spiflash", self.mem_map["spiflash"], self.lxspi.bus, size=16 * 1024 * 1024)
-        self.add_csr("lxspi")
+        spiflash_size = 16 * 1024 * 1024
+        self.submodules.spiflash = SpiFlash(platform.request("spiflash4x"), dummy=6, endianness="little")
+        self.register_mem("spiflash", self.mem_map["spiflash"], self.spiflash.bus, size=spiflash_size)
+        self.add_csr("spiflash")
+
+        # Add ROM linker region
+        self.add_memory_region("rom", self.mem_map["spiflash"] + flash_offset, spiflash_size - flash_offset, type="cached+linker")
 
         # In debug mode, add a UART bridge.  This takes over from the normal UART bridge,
         # however you can use the "crossover" UART to communicate with this over the bridge.
         if debug:
+            kwargs["uart_name"]   = "crossover"
             self.submodules.uart_bridge = UARTWishboneBridge(platform.request("serial"), clk_freq, baudrate=115200)
             self.add_wb_master(self.uart_bridge.wishbone)
             if hasattr(self, "cpu") and self.cpu.name == "vexriscv":
@@ -180,49 +174,45 @@ class BaseSoC(SoCCore):
             "icepack -s {build_name}.txt {build_name}.bin"
         ]
 
-    def generate_memory_x(self, destination):
-        write_to_file(os.path.join(destination, "memory.x"), get_memory_x(self.mem_regions, self.cpu.reset_address))
-
-
-def get_memory_x(mem_regions, cpu_reset_address):
-    r = export.get_linker_regions(mem_regions)
-    r += '\n'
-    r += 'REGION_ALIAS("REGION_TEXT", spiflash);\n'
-    r += 'REGION_ALIAS("REGION_RODATA", spiflash);\n'
-    r += 'REGION_ALIAS("REGION_DATA", sram);\n'
-    r += 'REGION_ALIAS("REGION_BSS", sram);\n'
-    r += 'REGION_ALIAS("REGION_HEAP", sram);\n'
-    r += 'REGION_ALIAS("REGION_STACK", sram);\n\n'
-    r += '/* CPU reset location. */\n'
-    r += '_stext = {:#08x};\n'.format(cpu_reset_address)
-    return r
-
 
 # Build --------------------------------------------------------------------------------------------
+
 def main():
     parser = argparse.ArgumentParser(description="LiteX SoC on iCEBreaker")
+    parser.add_argument("--flash-offset", default=0x40000, help="Boot offset in SPI Flash")
     parser.add_argument("--nextpnr-seed", default=0, help="Select nextpnr pseudo random seed")
     parser.add_argument("--nextpnr-placer", default="heap", choices=["sa", "heap"], help="Select nextpnr placer algorithm")
     parser.add_argument("--debug", action="store_true", help="Enable debug features. (UART has to be used with the wishbone-tool.)")
     parser.add_argument("--document-only", action="store_true", help="Do not build a soc. Only generate documentation.")
+    parser.add_argument("--flash", action="store_true", help="Load bitstream")
     builder_args(parser)
     soc_core_args(parser)
     args = parser.parse_args()
 
-    soc = BaseSoC(debug=args.debug, **soc_core_argdict(args))
+    # Create the SOC
+    soc = BaseSoC(debug=args.debug, flash_offset=args.flash_offset, **soc_core_argdict(args))
     soc.set_yosys_nextpnr_settings(nextpnr_seed=args.nextpnr_seed, nextpnr_placer=args.nextpnr_placer)
 
+    # Configure command line parameter defaults
     # Don't build software -- we don't include it since we just jump to SPI flash.
     builder_kwargs = builder_argdict(args)
     builder_kwargs["compile_software"] = False
+
     if args.document_only:
         builder_kwargs["compile_gateware"] = False
     if builder_kwargs["csr_svd"] is None:
         builder_kwargs["csr_svd"] = "../rust/icebesoc-pac/iCEBESOC.svd"
+    if builder_kwargs["memory_x"] is None:
+        builder_kwargs["memory_x"] = "../rust/icebesoc-pac/memory.x"
+
+    # Create and run the builder
     builder = Builder(soc, **builder_kwargs)
     builder.build()
-    soc.generate_memory_x("../rust/icebesoc-pac/")
     lxsocdoc.generate_docs(soc, "build/documentation/", project_name="iCEBreaker LiteX Riscv Example SOC", author="Piotr Esden-Tempski")
+
+    # If requested load the resulting bitstream onto the iCEBreaker
+    if args.flash:
+        IceStormProgrammer().flash(0x00000000, "soc_basesoc_icebreaker/gateware/top.bin")
 
 
 if __name__ == "__main__":

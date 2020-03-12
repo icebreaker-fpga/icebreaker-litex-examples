8429715a5c9ba63efe63ee1b40f0714c0342baca
Piotr Esden-Tempski <piotr@esden.net>
2020-03-12 14:37:56 -0700

    Updated c example to use 32bit CSRs.

--- c-riscv-blink/include/hw/common.h
+++ c-riscv-blink/include/hw/common.h
@@ -16,35 +16,16 @@
 #else /* ! __ASSEMBLER__ */
 #define MMPTR(x) (*((volatile unsigned int *)(x)))
 
-static inline void csr_write_simple(uint8_t value, uint32_t addr)
-{
-	*((volatile uint8_t *)addr) = value;
-}
-
-static inline uint8_t csr_read_simple(uint32_t addr)
-{
-	return *(volatile uint8_t *)addr;
-}
-
-static inline void csr_writew(uint16_t value, uint32_t addr)
-{
-	*((volatile uint16_t *)addr) = value;
-}
-
-static inline uint16_t csr_readw(uint32_t addr)
-{
-	return *(volatile uint16_t *)addr;
-}
-
-static inline void csr_writel(uint32_t value, uint32_t addr)
+static inline void csr_write_simple(uint32_t value, uint32_t addr)
 {
 	*((volatile uint32_t *)addr) = value;
 }
 
-static inline uint32_t csr_readl(uint32_t addr)
+static inline uint32_t csr_read_simple(uint32_t addr)
 {
 	return *(volatile uint32_t *)addr;
 }
+
 #endif /* ! __ASSEMBLER__ */
 
 #endif /* ! CSR_ACCESSORS_DEFINED */
--- c-riscv-blink/src/main.c
+++ c-riscv-blink/src/main.c
@@ -7,14 +7,14 @@ void isr(void) {
 
 
 int main(void) {
-    uint8_t x = 0;
+    uint32_t x = 0;
     while (1) {
         uart_rxtx_write('a');
         leds_out_write(x);
         if (x == 0x02) {
-            x = 0x01;
+            x = 0x00000001;
         } else {
-            x = 0x02;
+            x = 0x00000002;
         }
         msleep(80);
     }

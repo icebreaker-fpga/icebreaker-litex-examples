b831ee013b72d5e967b209bf0dc53e846acc455d
Greg Davill <greg.davill@gmail.com>
2021-10-13 15:30:16 +1030

    Update c-riscv-blink
    - Blink red/green while IDLE
    - Update generated includes

--- c-riscv-blink/src/main.c
+++ c-riscv-blink/src/main.c
@@ -12,28 +12,34 @@ void isr(void) {
 int main(void) {
     uint32_t x = 0;
     uint32_t buf = 0;
+
+    // time_init configures timer0 so we can use elapsed()
+    time_init();
+    int last_event = 0;
+    
     while (1) {
 
-        // Wait for an incoming byte
-        while (uart_rxempty_read()) {
-            // Prevent the while loop from being optimized out
-            asm("nop");
+        // Do we have an incoming byte?
+        if(!uart_rxempty_read()) {
+          // Read an incoming byte
+          buf = uart_rxtx_read();
+          // Tell the UART that we read a byte out of the FIFO
+          // and that it can give us another.
+          uart_ev_pending_write(UART_EV_RX);
+          // Mirror the bytes back
+          uart_rxtx_write(buf);
         }
 
-        // Read an incoming byte
-        buf = uart_rxtx_read();
-        // Tell the UART that we read a byte out of the FIFO
-        // and that it can give us another.
-        uart_ev_pending_write(UART_EV_RX);
-        // Mirror the bytes back
-        uart_rxtx_write(buf);
+        // Toggle LEDs every 250ms
+        if(elapsed(&last_event, (CONFIG_CLOCK_FREQUENCY * 250e-3))){
+          if (x == 2) {
+              x = 0x00000001;
+          } else {
+              x = 0x00000002;
+          }
+        }
 
-        // Write new LED output and toggle
+        // Write new LED output
         leds_out_write(x | (buf << 2));
-        if (x == 2) {
-            x = 0x00000001;
-        } else {
-            x = 0x00000002;
-        }
     }
 }
